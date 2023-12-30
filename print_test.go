4a3b57313f54355008ab4b31126c7107e8b4e3d3
Sean Gerhardt <sean_gerhardt@hotmail.com>
2023-12-29 19:50:17 -0600

    add func to print paths within world

--- print_test.go
+++ print_test.go
@@ -1,6 +1,12 @@
 package pathing
 
-import "testing"
+import (
+	"bytes"
+	"github.com/stretchr/testify/assert"
+	"io"
+	"os"
+	"testing"
+)
 
 func Test_printPath(t *testing.T) {
 	type args struct {
@@ -8,11 +14,13 @@ func Test_printPath(t *testing.T) {
 		p []tile
 	}
 	tests := []struct {
-		name string
-		args args
+		name           string
+		args           args
+		expectedOutput string
 	}{
 		{
-			name: "simple print",
+			name:           "simple print",
+			expectedOutput: "1 X \n3 X \n",
 			args: args{
 				w: populateWorld(t, simple),
 				p: []tile{{
@@ -26,7 +34,8 @@ func Test_printPath(t *testing.T) {
 				}},
 			},
 		}, {
-			name: "forest print",
+			name:           "forest print",
+			expectedOutput: "1 X X X \n1 8 8 X \n1 8 8 X \n1 1 1 X \n",
 			args: args{
 				w: populateWorld(t, forest),
 				p: []tile{{
@@ -59,7 +68,33 @@ func Test_printPath(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			// Create a pipe to capture stdout.
+			r, w, err := os.Pipe()
+			if err != nil {
+				t.Fatal(err)
+			}
+			originalStdout := os.Stdout
+			defer func() {
+				os.Stdout = originalStdout
+				w.Close()
+				r.Close()
+			}()
+
+			os.Stdout = w
+
 			printPath(tt.args.w, tt.args.p)
+
+			// Close the write end of the pipe to finish writing.
+			w.Close()
+
+			// Read the captured output from the read end of the pipe.
+			var buf bytes.Buffer
+			if _, err := io.Copy(&buf, r); err != nil {
+				t.Fatal(err)
+			}
+
+			// Check if the output is what you expect.
+			assert.Equal(t, tt.expectedOutput, buf.String())
 		})
 	}
 }
