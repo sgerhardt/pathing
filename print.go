0de4a3936a8fa4526796e15ed8e86083ed942e30
Sean Gerhardt <sean_gerhardt@hotmail.com>
2023-12-31 22:58:54 -0600

    fix map printing when path loops back to earlier rows

--- print.go
+++ print.go
@@ -2,17 +2,25 @@ package pathing
 
 import "fmt"
 
+type coordinates struct {
+	row int
+	col int
+}
+
 func printPath(w world, p []tile) {
 	if len(p) == 0 || len(w.tiles) == 0 {
 		return
 	}
+
+	var pathMap = make(map[coordinates]bool)
+	for _, t := range p {
+		pathMap[coordinates{t.col, t.row}] = true
+	}
+
 	for _, r := range w.tiles {
 		for _, c := range r {
-			if c.row == p[0].row && c.col == p[0].col {
+			if pathMap[coordinates{c.col, c.row}] {
 				fmt.Print("X")
-				if len(p) > 1 {
-					p = p[1:]
-				}
 			} else {
 				fmt.Print(c.value)
 			}
